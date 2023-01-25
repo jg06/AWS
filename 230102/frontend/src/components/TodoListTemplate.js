e6afaecd53ec7005aec68f95c80589c19747dee0
jg06 <jg06241@gmail.com>
2023-01-25 16:24:56 +0900

    project

--- 230102/frontend/src/components/TodoListTemplate.js
+++ 230102/frontend/src/components/TodoListTemplate.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./TodoListTemplate.css";
 
 const TodoListTemplate = ({ form, palette, children }) => {
+  console.log(children);
   return (
     <main className="todo-list-template">
       <div className="title">오늘 할 일</div>
